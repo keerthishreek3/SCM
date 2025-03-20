b5506ad489f8659ba3da6256ba3308e4e56493af
keerthishreek3 <keerthishree.k@s.amity.edu>
2025-03-20 22:05:28 +0530

    added printf and scanf statements

---
+++ switch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+	int choice,a,b;
+	printf("enter choice \n addition :press 1 \n substraction :press 2 \n multiplication: press 3 \n division:press 4\n");
+	scanf("%d",&choice);
+	printf ("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	
