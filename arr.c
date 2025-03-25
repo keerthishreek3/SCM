08010964dcc6098a286ee28ca1486bafe8e1bbc8
keerthishreek3 <keerthishree.k@s.amity.edu>
2025-03-25 21:03:38 +0530

    added printf statements

--- arr.c
+++ arr.c
@@ -5,6 +5,12 @@ int main()
 	scanf("%d",&n);
 	int arr[n]
 		for (int i=0;i<n;i++)
+		{
+			printf("enter the elements");
+			scanf("%d",&e);
+                    arr [i]=e
+			    printf("%d\n",e);
+		}
 return 0;
 }
 
