3669cd2492125457908b8e9d31e97972986a62fa
keerthishreek3 <keerthishree.k@s.amity.edu>
2025-04-02 20:18:10 +0530

    added the condition for swapping two numbers using temporary variable

--- pointer.c
+++ pointer.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 int main()
 {
-	int num1,num2,*ptr1,*ptr2;
+	int num1,num2,*ptr1,*ptr2,temp;
 	printf("enter the number:");
 	scanf("%d",&num1);
         printf("enter the number:");
 	scanf("%d",&num2);
 	*ptr1=&num1;
 	*ptr2=&num2;
+	temp=*ptr1;
+	*ptr1=*ptr2;
+	*ptr2=temp;
 
 
+        
