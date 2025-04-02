86586bb4c99ff5cf647d6129eecce7b373be167f
keerthishreek3 <keerthishree.k@s.amity.edu>
2025-04-02 20:15:26 +0530

    assigned the pointers with numbers

--- pointer.c
+++ pointer.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	int num1,num2;
+	int num1,num2,*ptr1,*ptr2;
 	printf("enter the number:");
-	scanf("%d",num1);
+	scanf("%d",&num1);
         printf("enter the number:");
-	scanf("%d",num2);
+	scanf("%d",&num2);
+	*ptr1=&num1;
+	*ptr2=&num2;
+
 
