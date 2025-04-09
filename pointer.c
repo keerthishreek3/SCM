5dbc2251ad958d95a60d5d5a094f0a43fa07ffdd
keerthishreek3 <keerthishree.k@s.amity.edu>
2025-04-09 13:39:16 +0530

    added the condition for sum of 2 numbers

--- pointer.c
+++ pointer.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int num1,num2,*ptr1,*ptr2;
+	int num1,num2,*ptr1,*ptr2,sum;
 	printf("enter the number");
 	scanf("%d",&num1);
 	printf("enter the number");
 	scanf("%d",&num2);
 	*ptr1=&num1;
 	*ptr2=&num2;
+	sum=*ptr1+*ptr2;
+	printf("sum of two numbers : %d \n,sum);
+	return 0;
+}
 
