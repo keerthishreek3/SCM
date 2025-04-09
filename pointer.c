c43d47f6f42ee2915c90650e57329ba57434833c
keerthishreek3 <keerthishree.k@s.amity.edu>
2025-04-09 13:36:49 +0530

    assigned the value of pointers

--- pointer.c
+++ pointer.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main()
 {
-	int num1,num2;
+	int num1,num2,*ptr1,*ptr2;
 	printf("enter the number");
 	scanf("%d",&num1);
 	printf("enter the number");
 	scanf("%d",&num2);
+	*ptr1=&num1;
+	*ptr2=&num2;
+
